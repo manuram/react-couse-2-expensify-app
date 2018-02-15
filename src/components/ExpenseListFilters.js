70ffa96e54b9d89e4fedc5328d06789f5975fb43
Manoj Ramachandran <ramachandranm@email.chop.edu>
2018-02-15 18:25:51 -0500

    Add expense summary to dashboard

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -49,7 +49,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    setTextFilter: (text) => dispatch(setTextFilte(text)),
+    setTextFilter: (text) => dispatch(setTextFilter(text)),
     sortByDate: () => dispatch(sortByDate()),
     sortByAmount: () => dispatch(sortByAmount()),
     setStartDate: (startDate) => dispatch(setStartDate(startDate)),
